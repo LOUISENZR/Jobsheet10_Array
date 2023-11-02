1d84d3f4b951a1154310009ebf33b037c06f20df
LOUISENZR <louisanaza@gmail.com>
2023-11-02 08:55:06 +0700

    Fourth commit

--- Bioskop16.java
+++ Bioskop16.java
@@ -21,8 +21,9 @@ public class Bioskop16 {
         System.out.printf("%s \t %s\n", penonton[0][0], penonton[0][1]);
 
         System.out.println(penonton.length);
-        for (int i = 0; i < penonton.length; i++){
-            System.out.println("Panjang baris ke-" + (i+1) + ": ");
-        }
+        for (int i = 0; i < penonton.length; i++) {
+            int panjangBaris = penonton[i].length;
+            System.out.println("Panjang baris ke-" + (i + 1) + ": " + panjangBaris);
     }
+}
 }
